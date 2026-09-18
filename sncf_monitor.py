9366525bfe0146dbd85dedab835ab79ac08e2576
Caldor2000 <106672655+Caldor2000@users.noreply.github.com>
2026-09-18 13:08:21 +0200

    Update sncf_monitor.py

--- sncf_monitor.py
+++ sncf_monitor.py
@@ -208,6 +208,10 @@ def process_delays(feed, routes, trips, state):
         total_trains += 1
 
         trip_id = trip_update.trip.trip_id
+        
+        if total_trains <= 5:
+    print("DEBUG TRIP ID :", trip_id)
+    print("DEBUG STATIQUE :", trips.get(trip_id))
 
         if not trip_id:
             continue
